452479d1c3277334d43991ec3a7321377d5393a1
Shin Jinwoo <bluesky0255@naver.com>
2018-04-25 16:06:53 +0900

    ADD contants class for preferences

---
+++ app/src/main/java/latera/kr/snowonmarch/util/Constants.java
@@ -0,0 +1,8 @@
+package latera.kr.snowonmarch.util;
+
+public class Constants {
+
+	public static final String PREFS_NAME = "som_prefs";
+	public static final String PREFS_KEY_INITIALIZED = "initialized";
+	public static final String PREFS_KEY_DATE_CONTACT_LAST_SYNCHRONIZED = "dateContactLastSynchronized";
+}
