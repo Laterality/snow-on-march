a9e07fd8a96fba40abd37c02c333bf0771cf5d16
Shin Jinwoo <bluesky0255@naver.com>
2018-04-27 17:01:17 +0900

    EDIT PersonDBO
    - Changed field names
    - Added method sorting messages it sent
    - Added date field which hold recently sent message's date

--- app/src/main/java/latera/kr/snowonmarch/dbo/PersonDBO.java
+++ app/src/main/java/latera/kr/snowonmarch/dbo/PersonDBO.java
@@ -1,31 +1,59 @@
 package latera.kr.snowonmarch.dbo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
+import io.realm.RealmList;
 import io.realm.RealmObject;
+import io.realm.Sort;
 import io.realm.annotations.Index;
 import io.realm.annotations.PrimaryKey;
 import io.realm.annotations.Required;
+import io.realm.internal.OsResults;
 
 public class PersonDBO extends RealmObject{
 
 	@PrimaryKey
-	private int mId;
+	private int id;
 	@Index
-	private String mName;
+	private String name;
 	@Required @Index
-	private String mPhoneNumber;
+	private String address;
+	private RealmList<MessageDBO> messagesSent;
+	private long recent;
 
 	public PersonDBO() { }
 
 	public PersonDBO(int id, String name, String phoneNumber) {
-		this.mId = id;
-		this.mName = name;
-		this.mPhoneNumber = phoneNumber;
+		this.id = id;
+		this.name = name;
+		this.address = phoneNumber;
+		this.messagesSent = new RealmList<>();
+		this.recent = -1;
 	}
 
-	public int getId() { return this.mId; }
-	public String getName() { return this.mName; }
-	public String getPhoneNumber() { return this.mPhoneNumber; }
+	public int getId() { return this.id; }
+	public String getName() { return this.name; }
+	public String getAddress() { return this.address; }
+	public RealmList<MessageDBO> getSentMessages() { return this.messagesSent; }
+	public MessageDBO getRecentMessage() { if (this.messagesSent.isEmpty()) {return null;} return this.messagesSent.last(); }
 
-	public void setName(String name) { this.mName = name; }
-	public void setPhoneNumber(String phoneNumber ) { this.mPhoneNumber = phoneNumber; }
+	public void setName(String name) { this.name = name; }
+	public void setAddress(String address) { this.address = address; }
+	public void addMessage(MessageDBO message) { this.messagesSent.add(message); if (message.getDate() > recent) { recent = message.getDate(); } sort(); }
+	public void addMessages(ListIterator<MessageDBO> messages) {
+		MessageDBO msg;
+		while(messages.hasNext()) {
+			msg = messages.next();
+			msg.setSender(this);
+			this.messagesSent.add(msg);
+			if (msg.getDate() > this.recent) { recent = msg.getDate(); }
+		}
+		sort();
+	}
+
+	private void sort() {
+		this.messagesSent.sort("dateSent", Sort.ASCENDING);
+	}
 }
