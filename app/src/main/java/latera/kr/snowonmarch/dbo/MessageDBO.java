f5d87146541ee3a26fee3eb41d39325b8ecc3d94
Shin Jinwoo <bluesky0255@naver.com>
2018-05-02 13:38:29 +0900

    EDIT message dbo
    - Added sent flag judeges this message is sent by me MessageDBO

--- app/src/main/java/latera/kr/snowonmarch/dbo/MessageDBO.java
+++ app/src/main/java/latera/kr/snowonmarch/dbo/MessageDBO.java
@@ -10,6 +10,7 @@ public class MessageDBO extends RealmObject {
 	private String body;
 	private long dateSent;
 	private PersonDBO sender;
+	private boolean sent;
 
 	public MessageDBO() { }
 
@@ -20,10 +21,19 @@ public class MessageDBO extends RealmObject {
 		this.sender = null;
 	}
 
+	public MessageDBO(String phoneNumber, String body, long date, boolean sent) {
+		this.address = phoneNumber;
+		this.body = body;
+		this.dateSent = date;
+		this.sender = null;
+		this.sent = sent;
+	}
+
 	public String getAddress() { return this.address; }
 	public String getBody() { return this.body; }
 	public long getDate() { return this.dateSent; }
 	public PersonDBO getSender() { return sender; }
+	public boolean isIsent() { return this.sent; }
 
 	public void setSender(PersonDBO sender) { this.sender = sender; }
 }
