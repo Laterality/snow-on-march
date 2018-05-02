7a4573cdcc3f721892bbefc8124ff77e83fe1851
Shin Jinwoo <bluesky0255@naver.com>
2018-05-02 15:31:07 +0900

    ADD Message log list

--- app/src/main/java/latera/kr/snowonmarch/activity/MessageLogActivity.java
+++ app/src/main/java/latera/kr/snowonmarch/activity/MessageLogActivity.java
@@ -4,16 +4,53 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
 import latera.kr.snowonmarch.R;
+import latera.kr.snowonmarch.adapter.MessageLogAdapter;
+import latera.kr.snowonmarch.dbo.MessageDBO;
+import latera.kr.snowonmarch.dbo.PersonDBO;
 
 public class MessageLogActivity extends AppCompatActivity {
+	public static final String ARG_ADDRESS_SENDER = "address_sender";
+
+	private RecyclerView rvMessage;
+
+	private String mAddress;
+	private Realm mRealm;
+
 
 	@Override
-	public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-		super.onCreate(savedInstanceState, persistentState);
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_message_log);
 
+		mRealm = Realm.getDefaultInstance();
 
+		rvMessage = findViewById(R.id.rv_message_log_messages);
+
+		mAddress = getIntent().getStringExtra(ARG_ADDRESS_SENDER);
+
+		setupRv();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mRealm.close();
+	}
+
+	private void setupRv() {
+		RealmResults<MessageDBO> result = mRealm.where(MessageDBO.class).equalTo("address", mAddress)
+				.sort("dateSent", Sort.ASCENDING)
+				.findAll();
+
+		MessageLogAdapter adapter = new MessageLogAdapter(result);
+		rvMessage.setAdapter(adapter);
+		rvMessage.setLayoutManager(new LinearLayoutManager(this));
 	}
 }
---
+++ app/src/main/java/latera/kr/snowonmarch/adapter/MessageLogAdapter.java
@@ -0,0 +1,50 @@
+package latera.kr.snowonmarch.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
+import latera.kr.snowonmarch.R;
+import latera.kr.snowonmarch.dbo.MessageDBO;
+
+public class MessageLogAdapter extends RealmRecyclerViewAdapter<MessageDBO, MessageLogAdapter.ViewHolder> {
+
+	public MessageLogAdapter(OrderedRealmCollection<MessageDBO> data) {
+		super(data, true);
+	}
+
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message_log, parent, false);
+		return new ViewHolder(v);
+	}
+
+	@Override
+	public void onBindViewHolder(ViewHolder holder, int position) {
+		final MessageDBO msg = getItem(position);
+		holder.tvText.setText(msg.getBody());
+
+		if (msg.isIsent()) {
+			holder.tvText.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+		}
+		else {
+			holder.tvText.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
+		}
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder {
+
+		TextView tvText;
+
+		public ViewHolder(View itemView) {
+			super(itemView);
+
+			tvText = itemView.findViewById(R.id.tv_item_message_log_text);
+		}
+	}
+}
