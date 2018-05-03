051d668f025805c0178c50a094f6e8733b441f71
Shin Jinwoo <bluesky0255@naver.com>
2018-05-03 15:21:44 +0900

    EDIT Group dbo
    - Added immortal flag column for Default Groups

--- app/src/main/java/latera/kr/snowonmarch/dbo/GroupDBO.java
+++ app/src/main/java/latera/kr/snowonmarch/dbo/GroupDBO.java
@@ -9,17 +9,23 @@ import io.realm.annotations.PrimaryKey;
 public class GroupDBO extends RealmObject {
 	@PrimaryKey
 	private int id;
+	private String name;
 	private RealmList<PersonDBO> people;
+	private boolean immortal;
 
 	public GroupDBO() { }
 
-	public GroupDBO(int id, RealmList<PersonDBO> people) {
+	public GroupDBO(int id, String name) {
 		this.id = id;
+		this.name = name;
 		this.people = new RealmList<>();
 		this.people.addAll(people);
+		immortal = id == 1;
 	}
 
 	public int getId() { return this.id; }
+	public String getName() { return this.name; }
 	public RealmList<PersonDBO> getPeople() { return this.people; }
-
+	public boolean isImmortal() { return this.immortal; }
+	public void addPerson(PersonDBO p) { this.people.add(p); }
 }
