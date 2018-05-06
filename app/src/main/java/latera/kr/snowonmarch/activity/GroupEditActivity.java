2a108482715e3c88b8f88eb0857ea76a163c8c24
Shin Jinwoo <bluesky0255@naver.com>
2018-05-06 17:21:28 +0900

    EDIT EditGroupActivity
    - As AddGroupDialog added.

--- app/src/main/java/latera/kr/snowonmarch/activity/GroupEditActivity.java
+++ app/src/main/java/latera/kr/snowonmarch/activity/GroupEditActivity.java
@@ -1,4 +1,79 @@
 package latera.kr.snowonmarch.activity;
 
-public class GroupEditActivity {
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import latera.kr.snowonmarch.R;
+import latera.kr.snowonmarch.adapter.EditGroupAdapter;
+import latera.kr.snowonmarch.dbo.GroupDBO;
+import latera.kr.snowonmarch.dialog.AddGroupDialog;
+
+public class GroupEditActivity extends AppCompatActivity implements AddGroupDialog.AddGroupDialogListener{
+
+	private static final String TAG = "GROUP_EDIT_ACTIVITY";
+	private static final String TAG_DIALOG_FRAGMENT_ADD_GROUP = "ADD_GROUP_DIALOG_FRAGMENT";
+
+	private RecyclerView rvGroups;
+	private FloatingActionButton fabAddGroup;
+
+	private Realm mRealm;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_edit_group);
+		mRealm = Realm.getDefaultInstance();
+
+		rvGroups = findViewById(R.id.rv_edit_group_groups);
+		fabAddGroup = findViewById(R.id.fab_edit_group_add_group);
+
+		fabAddGroup.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				AddGroupDialog dialog = new AddGroupDialog();
+				dialog.show(getSupportFragmentManager(), TAG_DIALOG_FRAGMENT_ADD_GROUP);
+			}
+		});
+
+		setUpRecyclerview();
+	}
+
+	private void setUpRecyclerview() {
+		// Find all groups except Default group(Home)
+		RealmResults<GroupDBO> groups = mRealm.where(GroupDBO.class)
+				.notEqualTo("id", 1)
+				.findAll();
+		EditGroupAdapter adapter = new EditGroupAdapter(groups, new EditGroupAdapter.OnItemClickListener() {
+			@Override
+			public void onClick(GroupDBO group) {
+				Log.d(TAG, "Clicked group: " + group.getId());
+			}
+		});
+		rvGroups.setAdapter(adapter);
+		rvGroups.setLayoutManager(new LinearLayoutManager(this));
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		try {
+			mRealm.close();
+		}
+		catch (Exception e) {
+			Log.e(TAG, e.getMessage());
+		}
+	}
+
+	@Override
+	public void onOk(String name, String color) {
+		Log.d(TAG, "We should group as named " + name + " with color " + color);
+	}
 }
--- app/src/main/java/latera/kr/snowonmarch/adapter/EditGroupAdapter.java
+++ app/src/main/java/latera/kr/snowonmarch/adapter/EditGroupAdapter.java
@@ -1,4 +1,61 @@
 package latera.kr.snowonmarch.adapter;
 
-public class EditGroupAdapter {
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmRecyclerViewAdapter;
+import latera.kr.snowonmarch.R;
+import latera.kr.snowonmarch.dbo.GroupDBO;
+
+public class EditGroupAdapter extends RealmRecyclerViewAdapter<GroupDBO, EditGroupAdapter.ViewHolder> {
+
+	private final OnItemClickListener mListener;
+
+	public EditGroupAdapter(@Nullable OrderedRealmCollection<GroupDBO> data, OnItemClickListener listener) {
+		super(data, true);
+		this.mListener = listener;
+	}
+
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		ViewHolder holder = new ViewHolder(LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.item_edit_group_list, parent, false));
+
+		return holder;
+	}
+
+	@Override
+	public void onBindViewHolder(ViewHolder holder, int position) {
+		final GroupDBO item = getItem(position);
+
+		holder.tvGroupName.setText(item.getName());
+
+		holder.itemView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (mListener != null) {
+					mListener.onClick(item);
+				}
+			}
+		});
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder {
+
+		TextView tvGroupName;
+
+		public ViewHolder(View itemView) {
+			super(itemView);
+			tvGroupName = itemView.findViewById(R.id.tv_item_group_list_group_name);
+		}
+	}
+
+	public interface OnItemClickListener {
+		public void onClick(GroupDBO group);
+	}
 }
