a8fd2fea51f295d25262eae410520685facdd269
Shin Jinwoo <bluesky0255@naver.com>
2018-05-08 14:36:08 +0900

    EDIT group member edit activity and its recycler adapter
    - Wrote bare bone codes.

--- app/src/main/java/latera/kr/snowonmarch/activity/GroupMemberEditActivity.java
+++ app/src/main/java/latera/kr/snowonmarch/activity/GroupMemberEditActivity.java
@@ -1,4 +1,94 @@
 package latera.kr.snowonmarch.activity;
 
-public class GroupMemberEditActivity {
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import java.security.acl.Group;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import latera.kr.snowonmarch.R;
+import latera.kr.snowonmarch.adapter.EditMemberAdapter;
+import latera.kr.snowonmarch.dbo.GroupDBO;
+import latera.kr.snowonmarch.dbo.PersonDBO;
+
+public class GroupMemberEditActivity extends AppCompatActivity {
+
+	public static final String ARG_GROUP_ID = "GROUP_ID";
+
+	private static final String TAG = "ACTIVITY_EDIT_MEMBER";
+	private static final int INVALID_GROUP_ID = -1;
+
+	private RecyclerView rvMembers;
+	private FloatingActionButton fabAddMember;
+
+	private int groupId;
+	private Realm mRealm;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		groupId = getIntent().getIntExtra(ARG_GROUP_ID, INVALID_GROUP_ID);
+		mRealm = Realm.getDefaultInstance();
+
+		if (groupId == INVALID_GROUP_ID) {
+			new AlertDialog.Builder(this)
+					.setMessage(R.string.text_invalid_access)
+					.setPositiveButton(R.string.text_ok, new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialogInterface, int i) {
+							dialogInterface.dismiss();
+						}
+					})
+					.create()
+					.show();
+		}
+
+		rvMembers = findViewById(R.id.rv_edit_group_member_members);
+		fabAddMember = findViewById(R.id.fab_edit_group_member_add_member);
+
+		fabAddMember.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// TODO: find person and add to group
+			}
+		});
+
+
+		setupRv();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		try {
+			mRealm.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setupRv() {
+		GroupDBO group = mRealm.where(GroupDBO.class)
+				.equalTo("id", groupId)
+				.findFirst();
+
+		EditMemberAdapter adapter = new EditMemberAdapter(group, new EditMemberAdapter.OnItemClickListener() {
+			@Override
+			public void onClick(PersonDBO person) {
+				Log.d(TAG, "clicked person: " + person.getName());
+			}
+		});
+		rvMembers.setAdapter(adapter);
+		rvMembers.setLayoutManager(new LinearLayoutManager(this));
+	}
 }
--- app/src/main/java/latera/kr/snowonmarch/adapter/EditMemberAdapter.java
+++ app/src/main/java/latera/kr/snowonmarch/adapter/EditMemberAdapter.java
@@ -1,4 +1,68 @@
 package latera.kr.snowonmarch.adapter;
 
-public class EditMemberAdapter {
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import io.realm.RealmRecyclerViewAdapter;
+import latera.kr.snowonmarch.R;
+import latera.kr.snowonmarch.dbo.GroupDBO;
+import latera.kr.snowonmarch.dbo.PersonDBO;
+
+public class EditMemberAdapter extends RealmRecyclerViewAdapter<PersonDBO, EditMemberAdapter.ViewHolder> {
+
+	private final OnItemClickListener mListener;
+
+	public EditMemberAdapter(@Nullable GroupDBO data, OnItemClickListener listener) {
+		super(data.getPeople(), true);
+		this.mListener = listener;
+	}
+
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		return new ViewHolder(LayoutInflater.from(parent.getContext())
+		.inflate(R.layout.item_edit_group_member, parent, false));
+	}
+
+	@Override
+	public void onBindViewHolder(ViewHolder holder, int position) {
+		final PersonDBO person = getItem(position);
+
+		holder.tvName.setText(person.getName());
+		if (person.getName().equals(person.getAddress())) {
+			holder.tvAddr.setVisibility(View.INVISIBLE);
+		}
+		else {
+			holder.tvAddr.setVisibility(View.VISIBLE);
+			holder.tvAddr.setText(person.getAddress());
+		}
+
+		holder.itemView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (mListener != null) {
+					mListener.onClick(person);
+				}
+			}
+		});
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder {
+
+		TextView tvName;
+		TextView tvAddr;
+
+		public ViewHolder(View itemView) {
+			super(itemView);
+			tvName = itemView.findViewById(R.id.tv_item_edit_group_member_name);
+			tvAddr = itemView.findViewById(R.id.tv_item_edit_group_member_address);
+		}
+	}
+
+	public interface OnItemClickListener {
+		public void onClick(PersonDBO person);
+	}
 }
