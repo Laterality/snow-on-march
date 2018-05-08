f4016ecaa9c7bfac14c336094ca0bda34388c4a0
Shin Jinwoo <bluesky0255@naver.com>
2018-05-08 16:50:35 +0900

    ADD Application class
    - For calling Realm.init().

---
+++ app/src/main/java/latera/kr/snowonmarch/util/MyApplication.java
@@ -0,0 +1,14 @@
+package latera.kr.snowonmarch.util;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class MyApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Realm.init(this);
+	}
+}
