f51aec7fa2cd202e4480af1a400d40b380b1a46d
Shin Jinwoo <bluesky0255@naver.com>
2018-05-25 22:44:50 +0900

    EDIT splash activity
    - Styled.

--- app/src/main/java/latera/kr/snowonmarch/activity/SplashActivity.java
+++ app/src/main/java/latera/kr/snowonmarch/activity/SplashActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import io.realm.Realm;
@@ -17,15 +19,13 @@ import latera.kr.snowonmarch.util.MySmsManager;
 import latera.kr.snowonmarch.util.OnSynchronizationFinishedListener;
 
 public class SplashActivity extends AppCompatActivity {
-	private static final long DELAY = 1500;
+	private static final long DELAY = 1000;
+
+	private ProgressBar pbSync;
+	private TextView tvSync;
 
 	private SharedPreferences mPrefs;
 	private SharedPreferences.Editor mEditor;
-
-	private TextView tvStateSms;
-	private TextView tvStateContact;
-
-
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -33,9 +33,8 @@ public class SplashActivity extends AppCompatActivity {
 
 		setContentView(R.layout.activity_splash);
 
-		tvStateSms = findViewById(R.id.tv_splash_state_sms);
-		tvStateContact = findViewById(R.id.tv_splash_state_contact);
-
+		pbSync = findViewById(R.id.pb_splash_sync_progress);
+		tvSync = findViewById(R.id.tv_splash_sync_state);
 
 		mPrefs = getPreferences(Context.MODE_PRIVATE);
 		mEditor = mPrefs.edit();
@@ -46,18 +45,18 @@ public class SplashActivity extends AppCompatActivity {
 		Realm.init(this);
 		if (!initialized) {
 			// 초기 설정
-			tvStateContact.setText("synchronizing...");
-			tvStateSms.setText("synchronizing...");
+
+			pbSync.setVisibility(View.VISIBLE);
+			tvSync.setVisibility(View.VISIBLE);
+			tvSync.setText(R.string.text_init_setting);
 
 			new MySmsManager(this, new OnSynchronizationFinishedListener() {
 				@Override
 				public void onFinish(int syncID) {
-					tvStateSms.setText("synchronized");
 
 					new MyContactManager(SplashActivity.this, new OnSynchronizationFinishedListener() {
 						@Override
 						public void onFinish(int syncID) {
-							tvStateContact.setText("synchronized");
 							// 초기 설정 후 init. flag를 true로 변경
 							mEditor.putBoolean(Constants.PREFS_KEY_INITIALIZED, true);
 							mEditor.apply();
